138bcedfbe777cb7856ae51913bf76e7d2cf517d
FirasAZ <firas.abuzahra2002@gmail.com>
2025-03-13 00:13:08 +0300

    now response gets rendered as expected, configured prettierrc to remove parent parenthesis for single arguments in arrow functions

--- src/pages/NewChat.jsx
+++ src/pages/NewChat.jsx
@@ -12,9 +12,7 @@ import 'github-markdown-css' // Import GitHub Markdown styles
 
 const NewChat = () => {
   const [prompt, setPrompt] = useState('')
-  const [response, setResponse] = useState(
-    '{"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:27.5136788Z","response":"\u003cthink\u003e","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:27.7062983Z","response":"\n\n","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:27.9097731Z","response":"\u003c/think\u003e","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:28.1022303Z","response":"\n\n","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:28.2871073Z","response":"Hello","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:28.4769745Z","response":"!","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:28.6782721Z","response":" How","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:28.9003853Z","response":" can","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:29.1304975Z","response":" I","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:29.3672203Z","response":" assist","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:29.6318319Z","response":" you","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:29.8871531Z","response":" today","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:30.1588471Z","response":"?","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:30.7693918Z","response":" 😊","done":false} {"model":"deepseek-r1:8b","created_at":"2025-03-12T09:29:31.0257039Z","response":"","done":true,"done_reason":"stop","context":[128011,6151,128012,128013,271,128014,271,9906,0,2650,649,358,7945,499,3432,30,27623,232],"total_duration":3789401700,"load_duration":77812100,"prompt_eval_count":4,"prompt_eval_duration":197000000,"eval_count":16,"eval_duration":3513000000}'
-  )
+  const [response, setResponse] = useState('What can I help with?')
   const [loading, setLoading] = useState(false)
 
   const sendPromptToOllama = async () => {
@@ -31,12 +29,16 @@ const NewChat = () => {
           prompt,
         }),
       })
+      if (res.status === 404) throw new Error('No AI model found!')
+
       if (!res.body) throw new Error('No response body!')
 
       const reader = res.body.getReader()
       const decoder = new TextDecoder()
 
       let partialResponse = ''
+
+      console.groupCollapsed('chunks')
       while (true) {
         const { value, done } = await reader.read()
         if (done) break
@@ -44,16 +46,20 @@ const NewChat = () => {
         const chunk = decoder.decode(value, { stream: true })
         const data = JSON.parse(chunk)?.response
         partialResponse += data
-        console.log(data)
+        console.log(JSON.parse(chunk))
         // partialResponse = partialResponse.replace('<think>', '')
-        // partialResponse = partialResponse.replace('</think>', '')
-        setResponse((prev) => prev + partialResponse)
+        // partialResponse = partialResponse.replace('</think>', '\n')
+        setResponse(partialResponse) // to render the final and complete response instead of gradually writing the response
+        // setResponse((prev) => prev + partialResponse) // shows the process gradually the response gets rendered by the model, results in a very long and repeated responses
       }
     } catch (err) {
       console.error('Streaming error:', err)
-      setResponse('Error fetching response.')
+      // append error message if error occured mid response
+      // instead of overwriting the inital response
+      setResponse(prev => (prev += err))
     } finally {
       setLoading(false)
+      console.groupEnd()
     }
   }
   return (
@@ -62,12 +68,10 @@ const NewChat = () => {
       <div className="flex justify-between items-center">
         <DropdownMenu>
           <DropdownMenuTrigger>
-            {/* change the below to something other than button */}
             <div className="flex gap-1 items-center p-2 rounded-lg hover:bg-neutral-700 opacity-75 text-lg">
               ChatGPT
               <ChevronDown />
             </div>
-            {/* <CustomShadcnTrigger Icon={ChevronDown} text='ChatGPT' className='w-auto' /> */}
           </DropdownMenuTrigger>
           <DropdownMenuContent className="ml-54 w-80 bg-neutral-700 border-neutral-500 text-white rounded-xl px-1 py-3">
             <DropdownMenuItem>
@@ -105,7 +109,7 @@ const NewChat = () => {
           {/* User Prompt */}
           <div className="text-right my-2">
             <span className="px-4 py-2 rounded-full bg-neutral-700">
-              how do i create a mask group in figma
+              {prompt}
             </span>
           </div>
           <div className="word-break mt-5">
@@ -120,7 +124,7 @@ const NewChat = () => {
           type="text"
           placeholder="Ask anything"
           className="rounded-xl bg-neutral-700 w-full h-full p-3 focus:outline-0 resize-none"
-          onChange={(e) => setPrompt(e.target.value)}
+          onChange={e => setPrompt(e.target.value)}
         />
         <button
           className="absolute bottom-2 right-2 p-2 rounded-full bg-white text-black hover:bg-neutral-300"
